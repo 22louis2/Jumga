de7012d7d7316a297b1f7e662533bd167c19294d
lesley okoduwa <45374070+oselesley@users.noreply.github.com>
2021-01-09 15:34:07 +0100

    first commit

---
+++ src/main/java/com/flutterwave/jumga/JumgaApplication.java
@@ -0,0 +1,13 @@
+package com.flutterwave.jumga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumgaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JumgaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flutterwave/jumga/JumgaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flutterwave.jumga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumgaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
